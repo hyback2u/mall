78b11f8241833ea13f1055a7727d14447c9e69d9
wangxl2022 <1919543837@qq.com>
2022-05-28 21:49:32 +0800

    style: format code

--- mall-product/src/main/java/com/wxl/mall/product/controller/AttrController.java
+++ mall-product/src/main/java/com/wxl/mall/product/controller/AttrController.java
@@ -4,12 +4,11 @@ import com.wxl.common.utils.PageUtils;
 import com.wxl.common.utils.R;
 import com.wxl.mall.product.entity.AttrEntity;
 import com.wxl.mall.product.service.AttrService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.Map;
-
 
 
 /**
@@ -22,14 +21,14 @@ import java.util.Map;
 @RestController
 @RequestMapping("product/attr")
 public class AttrController {
-    @Autowired
+    @Resource
     private AttrService attrService;
 
     /**
      * 列表
      */
     @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = attrService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -40,8 +39,8 @@ public class AttrController {
      * 信息
      */
     @RequestMapping("/info/{attrId}")
-    public R info(@PathVariable("attrId") Long attrId){
-		AttrEntity attr = attrService.getById(attrId);
+    public R info(@PathVariable("attrId") Long attrId) {
+        AttrEntity attr = attrService.getById(attrId);
 
         return R.ok().put("attr", attr);
     }
@@ -50,8 +49,8 @@ public class AttrController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody AttrEntity attr){
-		attrService.save(attr);
+    public R save(@RequestBody AttrEntity attr) {
+        attrService.save(attr);
 
         return R.ok();
     }
@@ -60,8 +59,8 @@ public class AttrController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody AttrEntity attr){
-		attrService.updateById(attr);
+    public R update(@RequestBody AttrEntity attr) {
+        attrService.updateById(attr);
 
         return R.ok();
     }
@@ -70,8 +69,8 @@ public class AttrController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] attrIds){
-		attrService.removeByIds(Arrays.asList(attrIds));
+    public R delete(@RequestBody Long[] attrIds) {
+        attrService.removeByIds(Arrays.asList(attrIds));
 
         return R.ok();
     }
