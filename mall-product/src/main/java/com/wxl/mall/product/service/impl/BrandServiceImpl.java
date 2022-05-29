a145bd853e7d22f4ca8b8e8f261c1d0b736896f2
wangxl2022 <1919543837@qq.com>
2022-05-29 16:03:20 +0800

    feat: 品牌管理页面配置模糊查询

--- mall-product/src/main/java/com/wxl/mall/product/service/impl/BrandServiceImpl.java
+++ mall-product/src/main/java/com/wxl/mall/product/service/impl/BrandServiceImpl.java
@@ -8,6 +8,7 @@ import com.wxl.common.utils.Query;
 import com.wxl.mall.product.dao.BrandDao;
 import com.wxl.mall.product.entity.BrandEntity;
 import com.wxl.mall.product.service.BrandService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -16,14 +17,38 @@ import java.util.Map;
 @Service("brandService")
 public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> implements BrandService {
 
+    /**
+     * 默认是没有任何条件直接检索所有, 现在需要配置模糊查询
+     *
+     * @param params params
+     * @return PageUtils
+     */
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        String key = (String) params.get("key");
+        QueryWrapper<BrandEntity> wrapper = new QueryWrapper<>();
+
+        // 不为空, 说明页面要进行模糊检索
+        if (StringUtils.isNotBlank(key)) {
+            wrapper.eq("brand_id", key).or()
+                    .like("name", key).or().like("descript", key);
+        }
+
         IPage<BrandEntity> page = this.page(
-                new Query<BrandEntity>().getPage(params),
-                new QueryWrapper<BrandEntity>()
+                new Query<BrandEntity>().getPage(params), wrapper
         );
 
         return new PageUtils(page);
     }
+
+//    @Override
+//    public PageUtils queryPage(Map<String, Object> params) {
+//        IPage<BrandEntity> page = this.page(
+//                new Query<BrandEntity>().getPage(params),
+//                new QueryWrapper<BrandEntity>()
+//        );
+//
+//        return new PageUtils(page);
+//    }
 
 }
