b29186f378d8f8a62a2c1c5095c635b89cae2033
wangxl2022 <1919543837@qq.com>
2022-06-02 22:34:50 +0800

    feat: 会员服务进入注册中心, 配置member网关路由策略

--- mall-member/src/main/java/com/wxl/mall/member/controller/MemberLevelController.java
+++ mall-member/src/main/java/com/wxl/mall/member/controller/MemberLevelController.java
@@ -1,20 +1,14 @@
 package com.wxl.mall.member.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.wxl.mall.member.entity.MemberLevelEntity;
-import com.wxl.mall.member.service.MemberLevelService;
 import com.wxl.common.utils.PageUtils;
 import com.wxl.common.utils.R;
+import com.wxl.mall.member.entity.MemberLevelEntity;
+import com.wxl.mall.member.service.MemberLevelService;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.Map;
 
 
 /**
@@ -27,7 +21,7 @@ import com.wxl.common.utils.R;
 @RestController
 @RequestMapping("member/memberlevel")
 public class MemberLevelController {
-    @Autowired
+    @Resource
     private MemberLevelService memberLevelService;
 
     /**
