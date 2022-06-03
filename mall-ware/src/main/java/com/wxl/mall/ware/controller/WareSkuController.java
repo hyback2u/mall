d496918da974c1614bce2b531922c7e84a72b088
wangxl2022 <1919543837@qq.com>
2022-06-03 21:23:49 +0800

    feat: wms:商品库存list接口检索条件add

--- mall-ware/src/main/java/com/wxl/mall/ware/controller/WareSkuController.java
+++ mall-ware/src/main/java/com/wxl/mall/ware/controller/WareSkuController.java
@@ -1,20 +1,14 @@
 package com.wxl.mall.ware.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.wxl.mall.ware.entity.WareSkuEntity;
-import com.wxl.mall.ware.service.WareSkuService;
 import com.wxl.common.utils.PageUtils;
 import com.wxl.common.utils.R;
+import com.wxl.mall.ware.entity.WareSkuEntity;
+import com.wxl.mall.ware.service.WareSkuService;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.Map;
 
 
 /**
@@ -27,14 +21,14 @@ import com.wxl.common.utils.R;
 @RestController
 @RequestMapping("ware/waresku")
 public class WareSkuController {
-    @Autowired
+    @Resource
     private WareSkuService wareSkuService;
 
     /**
      * 列表
      */
     @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = wareSkuService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -45,8 +39,8 @@ public class WareSkuController {
      * 信息
      */
     @RequestMapping("/info/{id}")
-    public R info(@PathVariable("id") Long id){
-		WareSkuEntity wareSku = wareSkuService.getById(id);
+    public R info(@PathVariable("id") Long id) {
+        WareSkuEntity wareSku = wareSkuService.getById(id);
 
         return R.ok().put("wareSku", wareSku);
     }
@@ -55,8 +49,8 @@ public class WareSkuController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody WareSkuEntity wareSku){
-		wareSkuService.save(wareSku);
+    public R save(@RequestBody WareSkuEntity wareSku) {
+        wareSkuService.save(wareSku);
 
         return R.ok();
     }
@@ -65,8 +59,8 @@ public class WareSkuController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody WareSkuEntity wareSku){
-		wareSkuService.updateById(wareSku);
+    public R update(@RequestBody WareSkuEntity wareSku) {
+        wareSkuService.updateById(wareSku);
 
         return R.ok();
     }
@@ -75,8 +69,8 @@ public class WareSkuController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] ids){
-		wareSkuService.removeByIds(Arrays.asList(ids));
+    public R delete(@RequestBody Long[] ids) {
+        wareSkuService.removeByIds(Arrays.asList(ids));
 
         return R.ok();
     }
--- mall-ware/src/main/java/com/wxl/mall/ware/service/impl/WareSkuServiceImpl.java
+++ mall-ware/src/main/java/com/wxl/mall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,16 +1,17 @@
 package com.wxl.mall.ware.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wxl.common.utils.PageUtils;
 import com.wxl.common.utils.Query;
-
 import com.wxl.mall.ware.dao.WareSkuDao;
 import com.wxl.mall.ware.entity.WareSkuEntity;
 import com.wxl.mall.ware.service.WareSkuService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 
 @Service("wareSkuService")
@@ -18,11 +19,19 @@ public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
-        IPage<WareSkuEntity> page = this.page(
-                new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
-        );
+        QueryWrapper<WareSkuEntity> queryWrapper = new QueryWrapper<>();
 
+        // 添加定制查询条件 skuId,wareId
+        String skuId = (String) params.get("skuId");
+        String wareId = (String) params.get("wareId");
+        if (StringUtils.isNotBlank(skuId) && !"0".equalsIgnoreCase(skuId)) {
+            queryWrapper.eq("sku_id", skuId);
+        }
+        if (StringUtils.isNotBlank(wareId) && !"0".equalsIgnoreCase(wareId)) {
+            queryWrapper.eq("ware_id", wareId);
+        }
+
+        IPage<WareSkuEntity> page = this.page(new Query<WareSkuEntity>().getPage(params), queryWrapper);
         return new PageUtils(page);
     }
 
