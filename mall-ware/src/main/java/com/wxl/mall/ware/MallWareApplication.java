b12cdee1b966b8ef97df7fb4c39a1c20943f6f64
wangxl2022 <1919543837@qq.com>
2022-06-03 20:45:34 +0800

    feat: wms服务入驻注册中心

--- mall-ware/src/main/java/com/wxl/mall/ware/MallWareApplication.java
+++ mall-ware/src/main/java/com/wxl/mall/ware/MallWareApplication.java
@@ -1,13 +1,17 @@
 package com.wxl.mall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @author wangxl
  * @since 2022/4/29 20:20
  */
+@EnableTransactionManagement
+@MapperScan("com.wxl.mall.ware.dao")
 @EnableDiscoveryClient
 @SpringBootApplication
 public class MallWareApplication {
