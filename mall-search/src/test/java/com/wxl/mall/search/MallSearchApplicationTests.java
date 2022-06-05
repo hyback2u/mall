090dd3629eee7ec34f15b9ff7d0dcc62ca40217c
wangxl2022 <1919543837@qq.com>
2022-06-05 14:24:20 +0800

    feat: 测试复杂检索功能 todo

--- mall-search/src/test/java/com/wxl/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/wxl/mall/search/MallSearchApplicationTests.java
@@ -8,8 +8,12 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +36,39 @@ public class MallSearchApplicationTests {
     public void contextLoads() {
         System.out.println("*************client: " + client);
     }
+
+
+    /**
+     * 测试复杂检索功能
+     * 检索地址中带有 mill 的人员年龄分布和平均薪资
+     */
+    @Test
+    public void complexIndexTest() throws Exception {
+        // 1、创建检索请求
+        SearchRequest searchRequest = new SearchRequest();
+        // 指定索引
+        searchRequest.indices("bank");
+        // 指定DSL, 检索条件
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        // 构造检索条件
+        searchSourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+//        searchSourceBuilder.from();
+//        searchSourceBuilder.size();
+//        searchSourceBuilder.aggregation();
+
+
+        searchRequest.source(searchSourceBuilder);
+
+        System.out.println("**********检索条件: " + searchSourceBuilder);
+
+        // 2、执行检索
+        SearchResponse searchResponse = client.search(searchRequest, MallElasticSearchConfig.COMMON_OPTIONS);
+
+        // 3、分析结果
+        System.out.println("***********分析结果: " + searchResponse.toString());
+    }
+
 
     @ToString
     @AllArgsConstructor
