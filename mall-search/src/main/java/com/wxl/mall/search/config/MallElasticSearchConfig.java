bc65abe136b0aa8aac566da7b9eb0f6c5166e5dc
wangxl2022 <1919543837@qq.com>
2022-06-05 13:53:08 +0800

    feat: 测试给es中存储数据

--- mall-search/src/main/java/com/wxl/mall/search/config/MallElasticSearchConfig.java
+++ mall-search/src/main/java/com/wxl/mall/search/config/MallElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.wxl.mall.search.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
@@ -10,13 +11,21 @@ import org.springframework.context.annotation.Configuration;
  * 分布式商城, 检索服务配置
  * 1、导入依赖
  * 2、编写配置, 给容器中注入RestHighLevelClient
- * 3、参考官方api进行开发
+ * 3、参考官方api进行开发 https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high.html
  *
  * @author wangxl
  * @since 2022/6/5 13:15
  */
 @Configuration
 public class MallElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+
+        COMMON_OPTIONS = builder.build();
+    }
 
     @Bean
     public RestHighLevelClient esRestClient() {
--- mall-search/src/test/java/com/wxl/mall/search/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/wxl/mall/search/MallSearchApplicationTests.java
@@ -1,11 +1,22 @@
 package com.wxl.mall.search;
 
+import com.alibaba.fastjson.JSON;
+import com.wxl.mall.search.config.MallElasticSearchConfig;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 /**
  * @author wangxl
@@ -21,4 +32,36 @@ public class MallSearchApplicationTests {
     public void contextLoads() {
         System.out.println("*************client: " + client);
     }
+
+    @ToString
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    static class User {
+        private String userName;
+        private String gender;
+        private Integer age;
+    }
+
+    /**
+     * 测试给es中存储数据
+     * 保存、更新 二合一
+     */
+    @Test
+    public void indexTest() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1"); // 数据的id
+//        indexRequest.source("username", "zhangSan", "age", 18, "gender", "男");
+        User user = new User("zhangsan", "man", 18);
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON); // 要保存的内容
+
+        // 执行操作
+        IndexResponse response = client.index(indexRequest, MallElasticSearchConfig.COMMON_OPTIONS);
+
+        // 提取有用的响应数据
+        System.out.println("***************response: " + response);
+    }
+
+
 }
