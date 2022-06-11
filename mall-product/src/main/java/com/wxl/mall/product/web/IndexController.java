f3924e5d2c2cd3e7540c19578901644fdbb56494
wangxl2022 <1919543837@qq.com>
2022-06-12 01:05:40 +0800

    feat: Redisson读写锁测试

--- mall-product/src/main/java/com/wxl/mall/product/web/IndexController.java
+++ mall-product/src/main/java/com/wxl/mall/product/web/IndexController.java
@@ -4,7 +4,9 @@ import com.wxl.mall.product.entity.CategoryEntity;
 import com.wxl.mall.product.service.CategoryService;
 import com.wxl.mall.product.vo.Catelog2VO;
 import org.redisson.api.RLock;
+import org.redisson.api.RReadWriteLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * 首页页面跳转Controller
@@ -27,6 +30,72 @@ public class IndexController {
 
     @Resource
     private RedissonClient redissonClient;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    /**
+     * Redisson读写锁测试
+     * 写
+     *
+     * ---> 读写锁, 保证一定能读到最新数据; 修改期间, 写锁是一个排他锁(互斥锁), 读锁是一个共享锁;
+     *      写锁没释放, 读就必须等待
+     *
+     * @see IndexController#readValue()
+     */
+    @GetMapping(value = "/write")
+    @ResponseBody
+    public String writeValue() {
+        RReadWriteLock lock = redissonClient.getReadWriteLock("rw-lock");
+
+        String str = "";
+
+        RLock rLock = lock.writeLock();
+
+        try {
+            // 改数据加写锁, 读数据加读锁
+            rLock.lock();
+            str = UUID.randomUUID().toString();
+            Thread.sleep(30000);
+            stringRedisTemplate.opsForValue().set("writeValue", str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 释放锁
+            rLock.unlock();
+        }
+
+        return str;
+    }
+
+    /**
+     * Redisson读写锁测试
+     * 读
+     *
+     * @see IndexController#writeValue()
+     */
+    @GetMapping(value = "/read")
+    @ResponseBody
+    public String readValue() {
+        RReadWriteLock lock = redissonClient.getReadWriteLock("rw-lock");
+
+        String str = "";
+
+        // 加读锁
+        RLock readLock = lock.readLock();
+        readLock.lock();
+
+        try {
+            str = stringRedisTemplate.opsForValue().get("writeValue");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            readLock.unlock();
+        }
+
+        return str;
+    }
 
 
     /**
